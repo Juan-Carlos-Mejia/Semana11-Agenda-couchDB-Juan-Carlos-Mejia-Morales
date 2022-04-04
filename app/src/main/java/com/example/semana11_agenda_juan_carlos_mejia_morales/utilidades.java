b0b02107da9f7d7d0d6685ec356071cbf8a70e3a
Juan-Carlos-Mejía <usis002221@ugb.edu.sv>
2022-04-03 21:10:09 -0600

    agenda parte 1

---
+++ app/src/main/java/com/example/semana11_agenda_juan_carlos_mejia_morales/utilidades.java
@@ -0,0 +1,6 @@
+package com.example.semana11_agenda_juan_carlos_mejia_morales;
+
+public class utilidades {
+    static String url_consulta = "http://192.168.43.177:5984/db_agenda/_design/agenda/_view/db_agenda";
+    static String url_mto = "http://192.168.43.177:5984/db_agenda/";
+}
